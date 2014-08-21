0da18af0d8f6ff027201bc2565b426896f868382
vagrant <vagrant@precise32.(none)>
2014-08-21 18:51:36 +0000

    Added jquery animations to landing page

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+echo "asdf"
+echo "asdf"
+"asdf"
+"
+echo "asdf"
+	echo "asdf"
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+int main(void)
+{
+}
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
---
+++ test2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+	#include <stdio.h>
+echo "asdf"
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+int main(void)
+{
+}
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
+int main(void)
