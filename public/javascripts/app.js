107b08e0a65aa7460a2365ac9a54dd5b5ed569ef
vagrant <vagrant@precise32.(none)>
2014-09-01 03:39:13 +0000

    Added an overlay to collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -186,7 +186,9 @@ require('./album');
  var buildAlbumThumbnail = function() {
     var template =
         '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png"/>'
+      + '  <div class="collection-album-image-container">'
+      + '    <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -202,24 +204,54 @@ require('./album');
    return $(template);
  };
 
-  var updateCollectionView = function() {
-   var $collection = $(".collection-container .row");
-   $collection.empty();
- 
-   for (var i = 0; i < 33; i++) {
-     var $newThumbnail = buildAlbumThumbnail();
-     $collection.append($newThumbnail);
-   }
- };
+ var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
+
+var updateCollectionView = function() {
+  var $collection = $(".collection-container .row");
+  $collection.empty();
+
+  for (var i = 0; i < 33; i++) {
+    var $newThumbnail = buildAlbumThumbnail();
+    $collection.append($newThumbnail);
+  }
+
+  var onHover = function(event) {
+     $(this).append(buildAlbumOverlay("/album.html"));
+   };
+
+     var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+
+   $collection.find('.collection-album-image-container').hover(onHover);
+}
+
 
  if (document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
   $(document).ready(function() {
-    
+    console.log("test");
     updateCollectionView();
      
   });
 }
+
+
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
