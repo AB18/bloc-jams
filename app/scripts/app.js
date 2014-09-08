23c7d3edce745281f282439fdf20302ce6f7b89f
vagrant <vagrant@precise32.(none)>
2014-09-08 21:36:50 +0000

    Added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -120,9 +120,15 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
 
   blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
+
+   SongPlayer.onTimeUpdate(function(event, time){
+     $scope.$apply(function(){
+       $scope.playTime = time;
+     });
+   });
  }]);
 
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
    var currentSoundFile = null;
    var trackIndex = function(album, song) {
      return album.songs.indexOf(song);
@@ -165,21 +171,29 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
          currentSoundFile.setTime(time);
        }
      },
+     onTimeUpdate: function(callback) {
+      return $rootScope.$on('sound:timeupdate', callback);
+    },
      setSong: function(album, song) {
        if (currentSoundFile) {
             currentSoundFile.stop();
        }
        this.currentAlbum = album;
        this.currentSong = song;
-          currentSoundFile = new buzz.sound(song.audioUrl, {
+
+       currentSoundFile = new buzz.sound(song.audioUrl, {
               formats: [ "mp3" ],
           preload: true
+       });
+
+       currentSoundFile.bind('timeupdate', function(e){
+        $rootScope.$broadcast('sound:timeupdate', this.getTime());
        });
  
       this.play();
      }
    };
- });
+ }]);
  blocJams.directive('slider', ['$document', function($document){
     
     var calculateSliderPercentFromMouseEvent = function($slider, event) {
@@ -274,3 +288,32 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
    }
  };
 }]);
+
+blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ })
